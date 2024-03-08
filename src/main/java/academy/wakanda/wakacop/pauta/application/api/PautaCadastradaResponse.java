54ffadcaf6cbfe4c5ccd28e144ff6ca54011e3d8
joan <joansantosreis@gmail.com>
2024-03-08 02:39:36 -0300

    implementa flow repository pauta

--- src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
@@ -1,5 +1,6 @@
 package academy.wakanda.wakacop.pauta.application.api;
 
+import academy.wakanda.wakacop.pauta.domain.Pauta;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -9,5 +10,9 @@ import java.util.UUID;
 @ToString
 public class PautaCadastradaResponse {
     private UUID id;
+
+    public PautaCadastradaResponse(Pauta pauta) {
+        this.id = pauta.getId();
+    }
 }
 
